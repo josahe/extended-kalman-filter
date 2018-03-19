730a7fdfb17825441b83e5870b7ca38cdd2970dc
josahe <josahe@users.noreply.github.com>
2018-03-19 09:31:30 +0000

    improved normalisation of angles and tidied code

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -14,36 +14,38 @@ KalmanFilter::KalmanFilter() {}
 KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &Q_in, MatrixXd &R_laser_in, MatrixXd &R_radar_in,
-                        MatrixXd &H_laser_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  Q_ = Q_in;
-  R_laser_ = R_laser_in;
-  R_radar_ = R_radar_in;
-  H_laser_ = H_laser_in;
+												MatrixXd &Q_in, MatrixXd &R_laser_in,
+												MatrixXd &R_radar_in, MatrixXd &H_laser_in) {
+	/**
+		* initialise the state
+	*/
+	x_ = x_in;
+	P_ = P_in;
+	F_ = F_in;
+	Q_ = Q_in;
+	R_laser_ = R_laser_in;
+	R_radar_ = R_radar_in;
+	H_laser_ = H_laser_in;
 }
 
 void KalmanFilter::Predict() {
-  /**
-    * predict the state
-  */
-  x_ = F_ * x_;
+	/**
+		* predict the state
+	*/
+	x_ = F_ * x_;
 
 	MatrixXd Ft = F_.transpose();
 	P_ = F_ * P_ * Ft + Q_;
-
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-    * update the state by using Kalman Filter equations
-  */
-  MatrixXd H = H_laser_;
-  MatrixXd R = R_laser_;
+	/**
+		* update the state by using Kalman Filter equations
+	*/
+	MatrixXd H = H_laser_;
+	MatrixXd R = R_laser_;
 
-  VectorXd z_pred = H * x_;
+	VectorXd z_pred = H * x_;
 	VectorXd y = z.head(2) - z_pred;
 	MatrixXd Ht = H.transpose();
 	MatrixXd S = H * P_ * Ht + R;
@@ -57,35 +59,32 @@ void KalmanFilter::Update(const VectorXd &z) {
 	long x_size = x_.size();
 	MatrixXd I = MatrixXd::Identity(x_size, x_size);
 	P_ = (I - K * H) * P_;
-
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-    * update the state by using Extended Kalman Filter equations
-  */
+	/**
+		* update the state by using Extended Kalman Filter equations
+	*/
+	MatrixXd H = H_radar_;
+	MatrixXd R = R_radar_;
 
-  // Linearise the non-linear measurement function using Jacobian matrix
-  try {
-    H_radar_ = tools_.CalculateJacobian(x_);
-  } catch (std::overflow_error e) {
-    Predict();
-    return;
-  }
+	// Linearise the non-linear measurement function using Jacobian matrix
+	try {
+		H_radar_ = tools_.CalculateJacobian(x_);
+	} catch (std::overflow_error e) {
+		Predict();
+		return;
+	}
 
-  MatrixXd H = H_radar_;
-  MatrixXd R = R_radar_;
+	float h0 = sqrt(pow(x_[0], 2) + pow(x_[1], 2));
+	float h1 = atan2(x_[1], x_[0]);
+	float h2 = (x_[0]*x_[2] + x_[1]*x_[3]) / h0;
 
-  float h0 = sqrt(pow(x_[0], 2) + pow(x_[1], 2));
-  float h1 = atan2(x_[1], x_[0]);
-  float h2 = (x_[0]*x_[2] + x_[1]*x_[3]) / h0;
-  if (h1 >= Pi_) {
-    h1 -= twoPi_;
-  } else if (h1 < -Pi_) {
-    h1 += twoPi_;
-  }
-  VectorXd z_pred(3);
-  z_pred << h0, h1, h2;
+	while (h1 > Pi_) { h1 -= twoPi_; }
+	while (h1 < -Pi_) {	h1 += twoPi_;	}
+
+	VectorXd z_pred(3);
+	z_pred << h0, h1, h2;
 	VectorXd y = z - z_pred;
 	MatrixXd Ht = H.transpose();
 	MatrixXd S = H * P_ * Ht + R;
@@ -99,5 +98,4 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
 	long x_size = x_.size();
 	MatrixXd I = MatrixXd::Identity(x_size, x_size);
 	P_ = (I - K * H) * P_;
-
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -10,12 +10,11 @@ Tools::Tools() {}
 Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-    * Calculate the RMSE here.
-  */
-
-  VectorXd rmse(4);
+															const vector<VectorXd> &ground_truth) {
+	/**
+		* Calculate the RMSE here.
+	*/
+	VectorXd rmse(4);
 	rmse << 0,0,0,0;
 
 	// check the validity of the following inputs:
@@ -23,8 +22,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	//  * the estimation vector size should equal ground truth vector size
 	if (estimations.size() == 0) {
 		cout << "Estimation vector is empty" << endl;
-	}
-	if (estimations.size() != ground_truth.size()) {
+	} else if (estimations.size() != ground_truth.size()) {
 		cout << "Vecor size does not match" << endl;
 	}
 
@@ -41,56 +39,53 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	//calculate the squared root
 	rmse = rmse.array().sqrt();
 
-  assert (~rmse.isZero());
+	assert (~rmse.isZero());
 	return rmse;
-
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-    * Calculate a Jacobian here.
-  */
+	/**
+		* Calculate a Jacobian here.
+	*/
+	MatrixXd Hj(3,4);
 
-  MatrixXd Hj(3,4);
+	//recover state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
 
-  //recover state parameters
-  float px = x_state(0);
-  float py = x_state(1);
-  float vx = x_state(2);
-  float vy = x_state(3);
+	//pre-compute some common calculations
+	float c1 = pow(px,2) + pow(py,2);
+	float c2 = sqrt(c1);
+	float c3 = vx*py - vy*px;
+	float c4 = pow(c1, 3/2);
 
-  //pre-compute some common calculations
-  float c1 = pow(px,2) + pow(py,2);
-  float c2 = sqrt(c1);
-  float c3 = vx*py - vy*px;
-  float c4 = pow(c1, 3/2);
+	//check division by zero or close to zero
+	if (c1 <= 0.001) {
+		throw std::overflow_error("Attempted divide by zero\n");
+	}
 
-  //check division by zero or close to zero
-  if (c1 <= 0.001) {
-    throw std::overflow_error("Attempted divide by zero\n");
-  }
+	//compute the Jacobian matrix elements
+	float dpdpx = px / c2;
+	float dpdpy = py / c2;
+	float dpdvx = 0;
+	float dpdvy = 0;
 
-  //compute the Jacobian matrix elements
-  float dpdpx = px / c2;
-  float dpdpy = py / c2;
-  float dpdvx = 0;
-  float dpdvy = 0;
+	float drdpx = -(py / c1);
+	float drdpy = px / c1;
+	float drdvx = 0;
+	float drdvy = 0;
 
-  float drdpx = -(py / c1);
-  float drdpy = px / c1;
-  float drdvx = 0;
-  float drdvy = 0;
+	float dPdpx = (py * c3) / c4;
+	float dPdpy = (px * c3) / c4;
+	float dPdvx = px / c2;
+	float dPdvy = py / c2;
 
-  float dPdpx = (py * c3) / c4;
-  float dPdpy = (px * c3) / c4;
-  float dPdvx = px / c2;
-  float dPdvy = py / c2;
+	//populate the Jacobian matrix
+	Hj << dpdpx, dpdpy, dpdvx, dpdvy,
+				drdpx, drdpy, drdvx, drdvy,
+				dPdpx, dPdpy, dPdvx, dPdvy;
 
-  //populate the Jacobian matrix
-  Hj << dpdpx, dpdpy, dpdvx, dpdvy,
-        drdpx, drdpy, drdvx, drdvy,
-        dPdpx, dPdpy, dPdvx, dPdvy;
-
-  return Hj;
-
+	return Hj;
 }
